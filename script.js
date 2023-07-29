bafbc5c56b7f826faf180c603c61bde7fdbe3d1d
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-07-29 08:42:21 -0300

    Portafolio casi listo

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeInElement = document.querySelector(".fade-in");
+  fadeInElement.style.opacity = 1; // Mostramos el contenido gradualmente
+
+  // Escuchamos el evento 'load' para eliminar el contenido de la clase 'fade-in'
+  window.addEventListener("load", function () {
+    fadeInElement.classList.remove("fade-in");
+  });
+});
